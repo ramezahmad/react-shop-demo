6c3551ad5ab7a2a0937ca3096c7f80aaf3fce83e
ramez <ramezhishamahmad@gmail.com>
2024-03-20 17:20:38 +0300

    Consume context API and link it to State

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Shop from "./components/Shop.jsx";
 import Product from "./components/Product.jsx";
 import { DUMMY_PRODUCTS } from "./dummy-products.js";
 
-import {CartContext} from './store/shopping-cart-context.jsx'
+import { CartContext } from "./store/shopping-cart-context.jsx";
 
 function App() {
   const [shoppingCart, setShoppingCart] = useState({
@@ -68,8 +68,13 @@ function App() {
     });
   }
 
+  const ctxValue = {
+    items: shoppingCart.items,
+    addItemToCart: handleAddItemToCart,
+  };
+
   return (
-    <CartContext.Provider value={{items:[]}}>
+    <CartContext.Provider value={ctxValue}>
       <Header
         cart={shoppingCart}
         onUpdateCartItemQuantity={handleUpdateCartItemQuantity}
