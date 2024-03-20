54e2b81054a593dcaaa95802c4740e880ab99e07
ramez <ramezhishamahmad@gmail.com>
2024-03-20 16:54:19 +0300

    using Context API

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,10 @@ import { useState } from "react";
 
 import Header from "./components/Header.jsx";
 import Shop from "./components/Shop.jsx";
-import Product from "./Product.jsx";
+import Product from "./components/Product.jsx";
 import { DUMMY_PRODUCTS } from "./dummy-products.js";
+
+import {CartContext} from './store/shopping-cart-context.jsx'
 
 function App() {
   const [shoppingCart, setShoppingCart] = useState({
@@ -67,7 +69,7 @@ function App() {
   }
 
   return (
-    <>
+    <CartContext.Provider value={{items:[]}}>
       <Header
         cart={shoppingCart}
         onUpdateCartItemQuantity={handleUpdateCartItemQuantity}
@@ -79,7 +81,7 @@ function App() {
           </li>
         ))}
       </Shop>
-    </>
+    </CartContext.Provider>
   );
 }
 
